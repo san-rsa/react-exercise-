9235a5e85f84989cd2bddbef1f38f542871e03f6
rahman sanuth <sanuthrahman@gmail.com>
2022-08-29 17:39:25 +0100

    eighth react ass es6-spread-operator-practice complete

--- 8 es6-spread-operator-practice/src/components/App.jsx
+++ 8 es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function App() {
+  
+
+  const [input, saveInput] = useState("");
+
+  const [list, saveList] = useState([]);
+
+
+
+  
+ function text(event) {
+  const st = event.target.value
+  saveInput(st)
+ }
+
+ function save() {
+  // const st = event.target.value;
+  saveList(p =>{
+     return [...p, input];
+  }, saveInput("")
+  ); saveInput("")
+   
+}
+
+
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
+        <input onChange={text} value={input} type="text" />
+        <button onClick={save}>
+          <span >Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {list.map(item => <li>{item}</li>)}
         </ul>
       </div>
     </div>
