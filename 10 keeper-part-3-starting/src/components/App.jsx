4cae0b24658effe2322ee259759901285b9f3a6c
rahman sanuth <sanuthrahman@gmail.com>
2022-08-30 16:01:29 +0100

    tenth react ass keeper-part-3-complete

--- 10 keeper-part-3-starting/src/components/App.jsx
+++ 10 keeper-part-3-starting/src/components/App.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+
+  const [notes , setNotes] = useState([]);
+
+
+  function add(note) {
+        setNotes(p => { return [...p, note]})
+  }
+
+
+  function setDell(id) {
+    setNotes(p => {
+      return p.filter((item, index) => {
+        return index !== id; 
+      })
+    })
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea 
+      add={add} />
+      {notes.map((single, index) => { 
+        return <Note key={index} id={index} title={single.title} content={single.content} delete={setDell} />})}
       <Footer />
     </div>
   );
--- 10 keeper-part-3-starting/src/components/CreateArea.jsx
+++ 10 keeper-part-3-starting/src/components/CreateArea.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+ function fNote(event) {
+  const {value, name} = event.target;
+  setNote(pre => {
+  
+     return {
+      ...note,
+      [name]: value
+     }
+  })
+ }
+
+function addNote(event) {
+  
+    props.add(note)
+
+  setNote({
+    title: "",
+    content: ""
+  })
+  event.preventDefault()
+}
+
+
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title"  onChange={fNote} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange={fNote}  value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={addNote}>Add</button>
       </form>
     </div>
   );
